fcf2d8a170dcfd415ac504512cf3c80c4c924bc6
libecrola <libecrola@gmail.com>
2023-01-12 19:23:00 +0100

    extended compatibility

--- home/scripts.js
+++ home/scripts.js
@@ -1,5 +1,4 @@
 // VARIABLES
-
 let percentage = 0;
 let animationComplete = false;
 
@@ -7,30 +6,41 @@ let animationComplete = false;
 const titleTopOffset = 323;
 
 $(function () {
-    window.onscroll = () => {
-        if (window.scrollY < titleTopOffset) {
-            percentage = window.scrollY / titleTopOffset;
-            animationComplete = false;
-        } else {
-            if (percentage == 1) {
-                animationComplete = true;
-            } else {
-                percentage = 1;
-            }
-        }
+    let animation = new Animations();
 
-        if (!animationComplete) {
-            let spacingTop = (30.03 - 30.03 * percentage).toString() + "vh";
-            let spacingLeft = (23.86 - 23.86 * percentage).toString() + "vw";
-            const fontHeight =
-                68 - 68 * percentage < 32 ? 32 : 68 - 68 * percentage;
-
-            document.getElementById("main-name").style.top = spacingTop;
-            document.getElementById("main-name").style.left = spacingLeft;
-            document.getElementById("main-name").style.fontSize =
-                fontHeight.toFixed() + "px";
-
-            // console.log(Math.round(percentage * 100) + "%");
-        }
-    };
+    animation.animateTitle();
 });
+
+class Animations {
+    constructor() {}
+
+    animateTitle() {
+        window.onscroll = () => {
+            if (window.scrollY < titleTopOffset) {
+                percentage = window.scrollY / titleTopOffset;
+                animationComplete = false;
+            } else {
+                if (percentage == 1) {
+                    animationComplete = true;
+                } else {
+                    percentage = 1;
+                }
+            }
+
+            if (!animationComplete) {
+                let spacingTop = (30.03 - 30.03 * percentage).toString() + "vh";
+                let spacingLeft =
+                    (23.86 - 23.86 * percentage).toString() + "vw";
+                const fontHeight =
+                    7 - 7 * percentage < 3.3 ? 3.3 : 7 - 7 * percentage;
+
+                document.getElementById("main-name").style.top = spacingTop;
+                document.getElementById("main-name").style.left = spacingLeft;
+                document.getElementById("main-name").style.fontSize =
+                    fontHeight.toFixed() + "vh";
+
+                // console.log(Math.round(percentage * 100) + "%");
+            }
+        };
+    }
+}
