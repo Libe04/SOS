01479604b064441051067b58b30e24f337c36f94
libecrola <libecrola@gmail.com>
2023-01-14 17:11:04 +0100

    sub-title animation

--- home/scripts.js
+++ home/scripts.js
@@ -9,6 +9,7 @@ $(function () {
     let animation = new Animations();
 
     animation.animateTitle();
+    animation.animateSubtitle();
 });
 
 class Animations {
@@ -37,10 +38,100 @@ class Animations {
                 document.getElementById("main-name").style.top = spacingTop;
                 document.getElementById("main-name").style.left = spacingLeft;
                 document.getElementById("main-name").style.fontSize =
-                    fontHeight.toFixed() + "vh";
+                    fontHeight.toString() + "vh";
 
                 // console.log(Math.round(percentage * 100) + "%");
             }
         };
+    }
+
+    animateSubtitle() {
+        let array = ["Web Developer", "IT Student", "Designer", "Programmer"];
+        let charIndex = 0;
+        let wordIndex = 1;
+        let str = "";
+        let word = array[0];
+        let delay = 200;
+
+        function printNextChar(element) {
+            str += element.charAt(charIndex);
+            charIndex++;
+            //console.log(str + "----" + wordIndex + "----" + charIndex);
+            $("#sub-title span:first").text(str);
+        }
+
+        function deleteChar(end) {
+            str = str.substring(0, end);
+            charIndex--;
+            //console.log(str + "----" + wordIndex + "----" + charIndex);
+            $("#sub-title span:first").text(str);
+        }
+
+        // function loopForward() {
+        //     setTimeout(() => {
+        //         printNextChar(word);
+
+        //         if (charIndex >= word.length && wordIndex < array.length) {
+        //             word = array[wordIndex];
+        //             wordIndex++;
+        //             charIndex = 0;
+        //             str = "";
+        //             delay = 1000;
+        //             loopForward();
+        //         } else if (
+        //             charIndex <= word.length &&
+        //             wordIndex <= array.length
+        //         ) {
+        //             delay = 200;
+        //             loopForward();
+        //         } else {
+        //             wordIndex = 0;
+        //             loopForward();
+        //         }
+        //     }, delay);
+        // }
+
+        function loopForward() {
+            setTimeout(() => {
+                printNextChar(word);
+
+                if (charIndex >= word.length && wordIndex < array.length) {
+                    delay = 1000;
+                    loopBack();
+                } else if (
+                    charIndex < word.length &&
+                    wordIndex <= array.length
+                ) {
+                    delay = 200;
+                    loopForward();
+                } else {
+                    wordIndex = 0;
+                    loopForward();
+                }
+            }, delay);
+        }
+
+        function loopBack() {
+            setTimeout(() => {
+                deleteChar(charIndex - 1);
+
+                if (charIndex == 0 && wordIndex < array.length) {
+                    word = array[wordIndex];
+                    wordIndex++;
+                    delay = 1000;
+                    loopForward();
+                } else if (
+                    charIndex <= word.length &&
+                    wordIndex <= array.length
+                ) {
+                    delay = 100;
+                    loopBack();
+                } else {
+                    loopBack();
+                }
+            }, delay);
+        }
+
+        loopForward();
     }
 }
